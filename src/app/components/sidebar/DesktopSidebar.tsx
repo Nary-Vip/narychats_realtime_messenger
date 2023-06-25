e9b130da7099a4ffb13d092acbef2f37ee7e5ed5
Nary-Vip <nary2vip@gmai.com>
2023-06-25 16:47:45 +0530

    Added Profile settings page and pic update

--- src/app/components/sidebar/DesktopSidebar.tsx
+++ src/app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react'
 import DesktopItem from './DesktopItem'
 import { User } from '@prisma/client'
 import Avatar from './Avatar'
+import SettingsModal from './SettingsModal'
 
 interface DesktopSidebarProps{
     currentUser: User
@@ -17,6 +18,8 @@ const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
     console.log(currentUser)
 
   return (
+    <>
+    <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={()=>{setIsOpen(false)}}/>
     <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:overflow-y-auto lg:w-20 xl:px-6 lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
         <nav className='flex flex-col justify-between mt-4'>
             <ul role='list' className="flex flex-col items-center space-y-1">
@@ -41,6 +44,7 @@ const DesktopSidebar:React.FC<DesktopSidebarProps> = ({currentUser}) => {
             </div>
         </nav>
     </div>
+    </>
   )
 }
 
---
+++ src/app/components/sidebar/SettingsModal.tsx
@@ -0,0 +1,102 @@
+import { User } from '@prisma/client'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import Modal from '../Modal'
+import Input from '../inputs/Inputs'
+import Image from 'next/image'
+import { CldUploadButton } from 'next-cloudinary'
+import Button from '../buttons'
+
+interface SettingsModalsProps{
+    isOpen: boolean
+    onClose: () => void
+    currentUser: User
+}
+
+const SettingsModal:React.FC<SettingsModalsProps> = ({
+    isOpen,
+    onClose,
+    currentUser
+}) => {
+
+    const router = useRouter()   
+    const [isLoading, setIsLoading] = useState(false)
+    const {
+        register, handleSubmit, setValue, watch, formState:{errors}
+    } = useForm<FieldValues>({
+        defaultValues:{
+            name: currentUser.name,
+            image: currentUser.image
+        }
+    })
+
+    const image = watch('image')
+
+    const handleUpload = (result:any)=>{
+        setValue('image', result?.info?.secure_url, {shouldValidate: true})
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = data => {
+        setIsLoading(true)
+        axios.post("/api/settings", data)
+        .then(()=>{
+            router.refresh()
+            onClose()
+        })
+        .catch(()=>{
+            toast.error("Something went wrong!")
+        })
+        .finally(()=>{
+            setIsLoading(false)
+        })
+    }
+
+  return (
+    <div>
+        <Modal onClose={onClose} isOpen={isOpen}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="space-y-12">
+                    <div className="pb-12 border-b border-gray-900/10">
+                        <h2 className="text-base font-semibold leading-7 text-gray-900">
+                            Profile
+                        </h2>
+                        <p className="mt-1 text-sm leading-6 text-gray-600">
+                            Edit your profile
+                        </p>
+                        <div className="flex flex-col mt-10 gap-y-8">
+                            <Input label="Name" id="name" register={register} errors={errors} disabled={isLoading} required/>
+                            <div>
+                                <label className='block text-sm font-medium leading-6 text-gray-900'>
+                                    Photo
+                                </label>
+                                <div className="flex items-center mt-2 gap-x-3"> 
+                                    <Image src={image || currentUser?.image || '/images/person.jpg'} className="rounded-full" alt="Profile Image" height={48} width={48}/>
+                                    <CldUploadButton options={{maxFiles:1}} onUpload={handleUpload} uploadPreset='s3eutor4'>
+                                        <Button disabled={isLoading} secondary type="button">
+                                            Change
+                                        </Button>
+                                    </CldUploadButton>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="flex items-center justify-end mt-6 gap-x-6">
+                        <Button disabled={isLoading} secondary type="button" onClick={onClose}>
+                            Cancel
+                        </Button>
+                        <Button disabled={isLoading} type="submit" onClick={onClose}>
+                            Update
+                        </Button>
+                    </div>
+                </div>
+            </form>
+        </Modal>
+    </div>
+  )
+}
+
+export default SettingsModal
